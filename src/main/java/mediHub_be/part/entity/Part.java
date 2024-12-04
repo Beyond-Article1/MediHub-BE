a745e7fb583ed0d5fccbfc0f51d576965ec8e428
SANGHYUN0519 <tkdgus0519@naver.com>
2024-12-04 10:30:09 +0900

    feat: #27 과 이름 엔티티 수정

--- src/main/java/mediHub_be/part/entity/Part.java
+++ src/main/java/mediHub_be/part/entity/Part.java
@@ -1,4 +1,30 @@
 package mediHub_be.part.entity;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "dept")
+@EntityListeners(AuditingEntityListener.class)
+@SQLDelete(sql = "UPDATE dept SET del_date = LOCALTIME WHERE part_seq = ?")
 public class Part {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "part_seq", nullable = false)
+    private long partSeq;
+
+    @Column(name = "dept_seq", nullable = false)
+    private long deptSeq;
+
+    @Column(name = "part_name", nullable = false)
+    private String partName;
+
+
 }
