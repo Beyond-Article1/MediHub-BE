be46d9dd1a8423bcd01a774496965695bf24e84c
PBEM22 <dlacofbs1229@gmail.com>
2024-12-10 18:54:05 +0900

    feat: #128 알림에 Flag 연결

--- src/main/java/mediHub_be/notify/service/NotifyService.java
+++ src/main/java/mediHub_be/notify/service/NotifyService.java
@@ -1,5 +1,6 @@
 package mediHub_be.notify.service;
 
+import mediHub_be.board.entity.Flag;
 import mediHub_be.notify.entity.NotiType;
 import mediHub_be.user.entity.User;
 
@@ -10,11 +11,11 @@ public interface NotifyService {
     /**
      * 단일 리시버 (게시글에 대한 댓글 발생시 받는 유저는 게시글 작성자 한 명)
      */
-    public void send(User sender, User receiver, NotiType notiType, String url);
+    public void send(User sender, User receiver, Flag flag, NotiType notiType, String url);
 
     /** 다중 리시버
      * - 케이스 공유시 팔로워들에게 알림 발생
      * - 게시글 작성시 팔로워들에게 알림 발생
      */
-    public void send(User sender, List<User> receivers, NotiType notiType, String url);
+    public void send(User sender, List<User> receivers, Flag flag, NotiType notiType, String url);
 }
