0e594aa8dda69763e69903462a7bb2131cdf8199
Pangtaek <zombie3632@gmail.com>
2024-12-12 20:12:20 +0900

    fix: #168 오류 수정

--- src/main/java/mediHub_be/cp/dto/ResponseCpSearchCategoryAndCpSearchCategoryDataDTO.java
+++ src/main/java/mediHub_be/cp/dto/ResponseCpSearchCategoryAndCpSearchCategoryDataDTO.java
@@ -10,11 +10,13 @@ public class ResponseCpSearchCategoryAndCpSearchCategoryDataDTO {
 
     private long cpSearchCategorySeq;           // cp 검색카테고리 번호
     private String cpSearchCategoryName;        // cp 검색카테고리명
-    private List<ResponseSimpleCpSearchCategoryDataDTO> cpSearchCategoryDataDtoList;    // 카테고리 별 데이터
+    private List<ResponseSimpleCpSearchCategoryDataDTO> cpSearchCategoryDataDtoList; // 카테고리 별 데이터
 
     @Builder
     public ResponseCpSearchCategoryAndCpSearchCategoryDataDTO(
             long cpSearchCategorySeq,
             String cpSearchCategoryName) {
+        this.cpSearchCategorySeq = cpSearchCategorySeq;
+        this.cpSearchCategoryName = cpSearchCategoryName;
     }
 }
