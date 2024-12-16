ca1b441ad2bd2091dd76ebace751abbb8761e8fc
SANGHYUN0519 <tkdgus0519@naver.com>
2024-12-16 16:20:01 +0900

    FEAT: #163 회원 레디스 적용 임시

--- src/main/java/mediHub_be/config/RedisConfig.java
+++ src/main/java/mediHub_be/config/RedisConfig.java
@@ -1,8 +1,10 @@
 package mediHub_be.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -10,9 +12,20 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
+    @Value("${mariadb.host}")
+    private String dbName;
+
+    @Value("${db.password}")
+    private String dbPassword;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory("localhost", 6379);
+        RedisStandaloneConfiguration redisConfiguration = new RedisStandaloneConfiguration();
+        redisConfiguration.setHostName(dbName);
+        redisConfiguration.setPort(6379);
+        redisConfiguration.setPassword(dbPassword);
+        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisConfiguration);
+        return lettuceConnectionFactory;
     }
 
     @Bean
